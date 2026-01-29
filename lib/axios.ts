d8e7e6486118680fa983bc54eeb1c67ec9dc9c11
Santiago Andres Suescun Beltran <nspes2020@gmail.com>
2026-01-29 18:07:00 -0500

    fixnig

--- lib/axios.ts
+++ lib/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Crear instancia de axios configurada
 const api = axios.create({
-  baseURL:"/" ,
+  baseURL:"/api" ,
   headers: {
     "Content-Type": "application/json",
   },
